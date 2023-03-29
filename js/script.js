02df674a9a6607d1f51667899ae65a78c9a428ba
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-03-29 11:33:24 -0300

    feito comando com script e adc emoji para cada nº

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 function contar() {// let para var tem diferença de scopo, fora desse bloco não vai existir!
-    let inicio  = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
+    let inicio = document.getElementById('txti');
+    let fim = document.getElementById('txtf');
+    let passo = document.getElementById('txtp');
+    let res = document.getElementById('res');
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert('[ERRO] Faltam dados!')
     } else {
-        alert('Tudo OK!')
+        res.innerHTML = 'Contando: '
+        //esse comando transforma ini em um number, fazendo a conversão.
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for (let c = i; c <= f; c += p) {//for significa,enquanto o contator for menor ou igual ao fim, ele vai receber ele mesmo + o passo.
+            res.innerHTML += `${c} \u{1F911}`//comando para converter emoji para js o original seria U+1F911.
+        }
     }
 }

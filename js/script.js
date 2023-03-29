189a051d625e8fcadb0c35b1eaa34b4e7ac3ee08
Murillo Cecilio <murilloceciliomoreira@gmail.com>
2023-03-29 11:53:50 -0300

    ajuste de bug quando utilizava 0 no passo

--- js/script.js
+++ js/script.js
@@ -12,9 +12,21 @@ function contar() {// let para var tem diferença de scopo, fora desse bloco nã
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for (let c = i; c <= f; c += p) {//for significa,enquanto o contator for menor ou igual ao fim, ele vai receber ele mesmo + o passo.
-            res.innerHTML += `${c} \u{1F449}`//comando para converter emoji para js o original seria U+1F449.
+        if (p <= 0) {
+            //resolvendo problema que travava o site
+            alert('Passo Inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {// valor do inicio for menor que o final
+            //contagem crescente
+            for (let c = i; c <= f; c += p) {//for significa,enquanto o contator for menor ou igual ao fim, ele vai receber ele mesmo + o passo.
+                res.innerHTML += `${c} \u{1F449}`//comando para converter emoji para js o original seria U+1F449.
+            }
+        } else {
+            //contagem descendente
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
         res.innerHTML += `\u{1F3C1}`//emoji só funciona entre crase ``
     }
